05a7abfeebc1b01a4f1eead651c6eef159371687
Dityo Kreshna <dityo.kreshna354@gmail.com>
2021-08-26 06:44:21 +0700

    delet
    delet

--- lib/style/text_style.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:ui';
-
-import 'package:flutter/material.dart';
-
-class MyDefaultTextStyle{
-  TextStyle texthead = TextStyle(color: Colors.black,fontWeight: FontWeight.bold, fontSize: 18);
-  TextStyle texth1 = TextStyle(color: Colors.black,fontWeight: FontWeight.bold, fontSize: 18);
-  TextStyle texth2 = TextStyle(color: Colors.black,fontWeight: FontWeight.bold, fontSize: 14);
-}

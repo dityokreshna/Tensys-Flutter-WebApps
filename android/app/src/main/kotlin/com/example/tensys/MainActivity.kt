98bc7d9309845a77c52b5c92e093b58250e97611
Dityo Kreshna <dityo.kreshna354@gmail.com>
2021-09-10 10:54:39 +0700

    delete
    delete

--- android/app/src/main/kotlin/com/example/tensys/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.tensys
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/service.dart/firebase_service.dart
+++
@@ -1,5 +0,0 @@
-// import 'package:firebase_database/firebase_database.dart';
-
-// class MyFirebase {
-//   DatabaseReference myDBReference = FirebaseDatabase.instance.reference().child("users").child("12345").once() as DatabaseReference;
-// }
